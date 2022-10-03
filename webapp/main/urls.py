7aa833b34a714fba803824a0c250b41e7beb0597
tonkla2545 <suriyakamonkaro@gmail.com>
2022-10-03 17:02:18 +0700

    codelab_proc

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,9 +1,12 @@
+from http import client
 from django.urls import path
 from .views import (
-    list_view, 
+    list_view,
+    client #new function in  views.py    
 )
 
 app_name = 'main'
 urlpatterns = [
     path('', list_view, name='home-list'),
+    path('join-now',client, name='hangout'),#new url route
 ]
